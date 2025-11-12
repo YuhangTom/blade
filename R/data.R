c16cbe449ed9bea79555bcb9e6e1b49c275b693f
YuhangTom <yhlin@iastate.edu>
2025-11-11 18:06:29 -0600

    cors_df for best case

--- R/data.R
+++ R/data.R
@@ -1,8 +1,9 @@
-#' Subsampled dataset of wire and blade cuts
+#' Dataset of wire and blade alignment cross-correlation (ccf)
 #'
-#' This dataset is a subsampled version of the original `x3p` wire and blade cuts data. The subsampling is performed at a factor of 10.
+#' This dataset is the alignment cross-correlation of `x3p` wire and blade cuts.
 #'
-#' @format A list of `x3p` objects, each representing a surface scan. The list includes:
-#' * T1AW-LO-R1: An `x3p` object representing the first surface scan.
-#' * T1AS-R1: An `x3p` object representing the second surface scan.
-"x3p_subsamples"
+#' @format A tibble objects with 3 variables:
+#' * lag: The lag of the alignment position.
+#' * ccf: The cross-correlation of the aligned position.
+#' * ns: The number of observations the ccf is based on.
+"cors_df"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,30 +1,6 @@
 ## code to prepare `x3p_subsamples` dataset
-library(purrr)
+wire_name <- "T1AW-LO-R1"
+blade_name <- "T1AS-R1"
 
-wire_names <- c(
-  "T1AW-LO-R1"
-)
-
-wire_dir <- wire_names %>%
-  paste0("../Wirecuts/scans/", .) %>%
-  paste0(".x3p")
-
-wire_subsamples <- purrr::map(wire_dir, x3ptools::x3p_read) %>%
-  purrr::map(x3ptools::x3p_average, b = 10) %>%
-  purrr::set_names(wire_names)
-
-blade_names <- c(
-  "T1AS-R1"
-)
-
-blade_dir <- blade_names %>%
-  paste0("../Wirecuts/sheet-scans/", .) %>%
-  paste0(".x3p")
-
-blade_subsamples <- purrr::map(blade_dir, x3ptools::x3p_read) %>%
-  purrr::map(x3ptools::x3p_average, b = 10) %>%
-  purrr::set_names(blade_names)
-
-x3p_subsamples <- c(wire_subsamples, blade_subsamples)
-
-usethis::use_data(x3p_subsamples, overwrite = TRUE)
+cors_df <- readr::read_rds(sprintf("../Wirecuts/sheet-wire-alignments-hh/%s_%s.rds", blade_name, wire_name))
+usethis::use_data(cors_df, overwrite = TRUE)
