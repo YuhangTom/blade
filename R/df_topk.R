73b91e5180b8e036b5a3ce175fd88324dabe81c1
YuhangTom <yhlin@iastate.edu>
2025-11-11 18:45:38 -0600

    Remove probs, add example

--- R/df_topk.R
+++ R/df_topk.R
@@ -4,24 +4,32 @@
 #'
 #' @param data A alignment data frame with two columns: `lag` and `ccf`.
 #' @param k A positive integer to control the number of resulting local maximums.
-#' @param probs A value between 0 and 1 to control the data used for clustering. (XXX...Rephrase)
 #' @param len_block A positive number controlling the size of neighborhood.
 #' @return A data frame with three columns:
 #' * .cluster: The cluster id.
 #' * lag: The lag in the alignment.
 #' * ccf: The cross-correlation value in the alignment.
 #' @import dplyr
+#' @import ggplot2
 #' @importFrom dbscan dbscan augment
 #' @importFrom stats quantile
 #' @export
+#' @examples
+#' library(ggplot2)
+#' topk <- df_topk(cors_df)
+#' cors_df %>%
+#'   ggplot(aes(x = lag, y = ccf)) +
+#'   geom_point() +
+#'   geom_point(aes(color = "topk"), data = topk) +
+#'   theme_bw()
 #'
-df_topk <- function(data, k = 10, probs = 0.5, len_block = 100) {
+df_topk <- function(data, k = 10, len_block = 100) {
   .cluster <-
     ccf <-
     NULL
 
   data_top <- data %>%
-    filter(ccf > quantile(data$ccf, probs))
+    filter(ccf > 0)
 
   db_fit <- dbscan(data_top %>% select(lag),
     minPts = 1,
